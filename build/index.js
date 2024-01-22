5d27cb7b66bf438de6115c473de08aa4fb4cba15
jurifauser1395 <jurifauser@pm.me>
2024-01-22 19:48:48 +0100

    chagne build dir in tsconfig.json

--- build/index.js
+++ build/index.js
@@ -1,10 +1,2 @@
 "use strict";
-// interfaces
-const jf = { dbid: 4, email: 'f@n', user: 3,
-    startTrail: (user, googleId) => 4
-};
-const jfAdmin = {
-    dbid: 45, email: 'vre@', user: 4,
-    startTrail: (v, a) => 5,
-    role: 'admin'
-};
+console.log('hi');
