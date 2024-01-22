ced568298ebcc2d9b89c7378cef7392bbf5f9c24
jurifauser1395 <jurifauser@pm.me>
2024-01-22 20:15:52 +0100

    practice interfaces

--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('hi');
+function newBike(bike) {
+    console.log(' Checkout my new bike, it is ' + bike.color + ' and costs ' + bike.price + '$ and is ' + bike.shape + '!');
+}
+let myNewBike = {
+    shape: 'nice',
+    color: 'black',
+    price: 453,
+    weight: 34
+};
+newBike(myNewBike);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hi')
+interface Bike {
+    shape: string,
+    color: string,
+    price: number,
+    weight: number
+}
+
+function newBike (bike: Bike) {
+    console.log(' Checkout my new bike, it is ' + bike.color + ' and costs ' + bike.price + '$ and is ' + bike.shape + '!')
+}
+
+let myNewBike: Bike = {
+    shape: 'nice',
+    color: 'black',
+    price: 453,
+    weight: 34
+}
+newBike(myNewBike)
