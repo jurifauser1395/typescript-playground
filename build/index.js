352323b974659de42071a1a5fb33df5dbb9c211c
jurifauser1395 <jurifauser@pm.me>
2024-01-26 14:20:37 +0100

    further narrowing examples

--- build/index.js
+++ build/index.js
@@ -5,3 +5,26 @@ function isAdmin(account) {
         return account.isAdmin;
     }
 }
+// instanceof keyword 
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+// as keyword for typecasting 
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet.swim();
+        return 'fish food';
+    }
+    else {
+        pet.fly();
+        return 'bird food';
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,32 @@ function isAdmin(account: User | Admin) {
         return account.isAdmin
     }
 }
+
+
+// instanceof keyword 
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString())
+    } else {
+        console.log(x.toUpperCase())
+    }
+}
+
+type Fish = {swim: () => void}
+type Bird = {fly: () => void}
+
+
+// as keyword for typecasting 
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird) {
+    if (isFish(pet)) {
+        pet.swim()
+        return 'fish food'
+    } else {
+        pet.fly()
+        return 'bird food'
+    }
+}
