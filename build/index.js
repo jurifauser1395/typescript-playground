88aa1de0e241a8c5e26d46583415866319635cf5
jurifauser1395 <jurifauser@pm.me>
2024-01-26 14:05:51 +0100

    use 'in' keyword for narrowing types

--- build/index.js
+++ build/index.js
@@ -1,65 +1,7 @@
 "use strict";
-// generics in typescript
-const score = [];
-const names = [];
-function identityOne(val) {
-    return val;
-}
-function identityTow(val) {
-    return val;
-}
-function identityThree(val) {
-    return val;
-}
-// identityThree('3')
-function identityFour(val) {
-    return val;
-}
-identityFour({ brand: 'sr', type: 4 });
-function getSearchProducts(productId) {
-    return productId[3];
-}
-const getMoreSearchProducts = (productsID) => {
-    // do some database operations 
-    const myIndex = 4;
-    return productsID[myIndex];
-};
-function anotherFunction(valOne, valTwo) {
-    return {
-        valOne,
-        valTwo
-    };
-}
-anotherFunction(4, { connection: 'fv', username: 'v', password: 'vre' });
-class Sellable {
-    constructor() {
-        this.card = [];
-    }
-    addToCard(product) {
-        this.card.push(product);
+// The "in" keyword, very important 
+function isAdmin(account) {
+    if ('isAdmin' in account) {
+        return account.isAdmin;
     }
 }
-function detectType(val) {
-    if (typeof val === 'string') {
-        return val.toLowerCase();
-    }
-    return val + 43;
-}
-// null is a datatype! be careful!
-function provideID(id) {
-    if (!id) {
-        console.log('Please provide id!');
-        return;
-    }
-    id.toLowerCase();
-}
-let numbers = [3, 43, 34, 435, 43, 1];
-function loopTrough(n) {
-    let l = (n.length - 1);
-    if (Array.isArray(n)) {
-        for (let index of n) {
-            console.log('This is my number ' + index);
-        }
-    }
-}
-loopTrough(numbers);
--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,17 @@
-// generics in typescript
-
-const score: Array<number> = []
-
-const names: Array<string> = []
-
-function identityOne(val: boolean | number): boolean | number{
-    return val
-}
-
-function identityTow(val: any): any{
-    return val
-}
-
-function identityThree<Type>(val: Type): Type{
-    return val
-}
-
-// identityThree('3')
-
-function identityFour<T>(val: T): T {
-    return val
-}
-
-interface Bootle{
-    brand: string,
-    type: number
-}
-
-identityFour<Bootle>({brand: 'sr', type: 4})
-
-function getSearchProducts<T,>(productId: T[]): T{
-    return productId[3]
-}
-
-
-const getMoreSearchProducts = <T,>(productsID: T[]): T => {
-    // do some database operations 
-    const myIndex = 4
-    return productsID [myIndex]
-}
-
-
-interface Database {
-    connection: string,
-    username: string,
-    password: string
-}
-
-function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
-    return{
-        valOne,
-        valTwo
-    }
-}
-
-anotherFunction(4, {connection: 'fv', username: 'v', password: 'vre'})
-
-
-interface Quiz{
+interface User {
     name: string,
-    type: string
+    email: string
 }
 
-interface Course {
+interface Admin {
     name: string,
-    author: string,
-    subject: string
+    email: string,
+    isAdmin: boolean
 }
 
-
-class Sellable <T> {
-    public card: T[] = []
-
-    addToCard(product: T) {
-        this.card.push(product)
+// The "in" keyword, very important 
+function isAdmin(account: User | Admin) {
+    if ('isAdmin' in account) {
+        return account.isAdmin
     }
 }
-
-function detectType(val: number | string): number | string {
-    if(typeof val === 'string') {
-      return val.toLowerCase()
-    }
-
-    return val + 43
-}
-
-// null is a datatype! be careful!
-function provideID(id: string | null) {
-    if(!id) {
-        console.log('Please provide id!')
-        return
-    }
-    id.toLowerCase()
-}
-
-let numbers = [3, 43, 34, 435, 43, 1]
-
-function loopTrough <T> (n: T[]) {
-    
-    let l: number = (n.length - 1)
-    if(Array.isArray(n)) {
-    
-        for (let index of n) {
-            console.log('This is my number ' + index)
-        }
-    }
-} 
-
-loopTrough(numbers)
