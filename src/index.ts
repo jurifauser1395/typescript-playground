10263275031d4ffc6fd9434fc25995182078976c
jurifauser1395 <jurifauser@pm.me>
2024-01-28 16:06:09 +0100

    decoratro implementation in TS5.0

--- src/index.ts
+++ src/index.ts
@@ -89,11 +89,33 @@ function getArea(shape: Shape) {
     }
 }
 
-// Decorators
+function loggedMethod(originalMethod: any, context: ClassMethodDecoratorContext) {
+    const methodName = String(context.name);
 
+    function replacementMethod(this: any, ...args: any[]) {
+        console.log(`LOG: Entering method '${methodName}'.`)
+        const result = originalMethod.call(this, ...args);
+        console.log(`LOG: Exiting method '${methodName}'.`)
+        return result;
+    }
 
-function getAge(): string  {
-    return 'hi'
+    return replacementMethod;
 }
 
+// Decorators
+
+class Person {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    @loggedMethod
+    greet() {        
+        console.log(`Hello, my name is ${this.name}`);
+    }
+}
+
+const p = new Person('Juri');
+p.greet();
 
